44da27dfc3adf88a33ba774748202683573dfd15
enrique <jesus.aleman@uni-bielefeld.de>
2025-09-10 18:20:33 +0200

    complete temi setup

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 SESSION="temi_ros2"
 
+
 # Kill existing session if already running
 tmux kill-session -t $SESSION 2>/dev/null
 
 # Create new session (detached)
-tmux new-session -d -s $SESSION
+tmux new-session -d -s $SESSION -n "bringup"
 
 # Pane 1: ros2 bringup
 tmux send-keys -t $SESSION "ros2 launch temi_bringup bringup.launch.py namespace:=temi" C-m
@@ -15,11 +16,11 @@ tmux send-keys -t $SESSION "ros2 launch temi_bringup bringup.launch.py namespace
 tmux new-window -t $SESSION -n "gallery"
 tmux send-keys -t $SESSION:1 "python3 /home/temi/gallery_dir/app.py" C-m
 
-tmux new-window -t $SESSION -n "person_detection"
-tmux send-keys -t $SESSION:2 "ros2 run person_detection_ros person_detection_node --ros-args -p use_sim_time:=false" C-m
+tmux new-window -t $SESSION -n "detection"
+tmux send-keys -t $SESSION:2 "ros2 launch person_detection_ros detect.launch.py use_sim_time:=false use_rviz:=false namespace:=temi" C-m
 
-tmux new-window -t $SESSION -n "pdaf_tracker"
-tmux send-keys -t $SESSION:3 "ros2 launch pdaf_tracker track.launch.py" C-m
+tmux new-window -t $SESSION -n "tracker"
+tmux send-keys -t $SESSION:3 "ros2 launch pdaf_tracker track.launch.py namespace:=temi use_sim_time:=false" C-m
 
 # Attach to session
 tmux attach-session -t $SESSION
